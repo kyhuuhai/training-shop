8732f5ebfd5e7e1da59aed296b7927b038b2014e
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2022-04-26 14:14:29 +0700

    avatar-update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ActiveRecord::Base
-  
+  has_one_attached :avatar
   has_many :rates
   has_many :comments
   has_many :orders
-  has_many :images, as: :imageable
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
-  
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -78,14 +77,14 @@ class User < ActiveRecord::Base
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-  
+
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
-  
+
   # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
@@ -96,7 +95,7 @@ class User < ActiveRecord::Base
   def downcase_email
     self.email = email.downcase
   end
-  
+
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
