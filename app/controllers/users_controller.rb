f0b76ae25a5a53c09bbf1e7fc83f3e92cc754df4
“theduc151” <nguyentheduc15196@gmail.com>
2022-05-09 10:05:47 +0700

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:info] = "Please check your email to activate your account."
       redirect_to @user
     else
